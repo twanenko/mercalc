09db7d708c51e63697ecc73877e4c429be1a8ff9
twanenko <twanenko@icloud.com>
2020-04-28 15:12:42 +0900

    Updates

---
+++ precache-manifest.6594771902688ea2a655d95567eb8cf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ea6a0c8b822305126cb692dfe4c6f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44dfac38719893266929",
+    "url": "/static/css/main.cdadfa16.chunk.css"
+  },
+  {
+    "revision": "11d7c9ee990089d59380",
+    "url": "/static/js/2.219a79b6.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.219a79b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44dfac38719893266929",
+    "url": "/static/js/main.65033620.chunk.js"
+  },
+  {
+    "revision": "1291567a845f4021591f",
+    "url": "/static/js/runtime-main.5c60c111.js"
+  }
+]);
